6854b29b7f7e9a864209f07f95dbb128ca0ef1ad
Richard Tae <taerichard@gmail.com>
2022-10-25 00:42:44 -0700

    added react scroll from nav, changed form position, changed nav items desktop view, working on header

--- src/App.js
+++ src/App.js
@@ -12,70 +12,12 @@ import Title from "./components/Title";
 import Services from "./components/services/Services";
 import Products from "./components/products/Products";
 import Form from "./components/contact/Form";
-import { Link } from "react-scroll";
 
 function App() {
   return (
     <div className="container">
-      <header className="nav">
-        <nav className="nav-items">
-          <Link
-            className="nav-item"
-            to="about-wrapper"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={1000}
-          >
-            About
-          </Link>
-          <Link
-            className="nav-item"
-            to="products-wrapper"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={1000}
-          >
-            Products
-          </Link>
-          <Link
-            className="nav-item"
-            to="services-wrapper"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={1000}
-          >
-            Services
-          </Link>
-          <Link
-            className="nav-item"
-            to="contact-wrapper"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={1000}
-          >
-            Contact
-          </Link>
-        </nav>
-      </header>
-      <div id="about-wrapper">
-        <Title />
-      </div>
-      <div id="products-wrapper">
-        <Products />
-      </div>
-      <div id="services-wrapper">
-        <Services />
-      </div>
-      <div id="contact-wrapper">
-        <Form />
-      </div>
-
       <Routes>
-        {/* <Route path="/" element={<Main />} /> */}
+        <Route path="/" element={<Main />} />
         <Route path="accessories" element={<Accessories />} />
         <Route path="disposables" element={<Disposables />} />
         <Route path="ecigs" element={<Ecigs />} />
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,14 +2,69 @@ import Title from "../components/Title";
 import Services from "../components/services/Services";
 import Products from "../components/products/Products";
 import Form from "../components/contact/Form";
+import { Link } from "react-scroll";
 
 const Main = () => {
   return (
     <div>
-      <Title />
-      <Products />
-      <Services />
-      <Form />
+      <div>
+        <header className="nav">
+          <nav className="nav-items">
+            <Link
+              className="nav-item"
+              to="about-wrapper"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={1000}
+            >
+              About
+            </Link>
+            <Link
+              className="nav-item"
+              to="products-wrapper"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={1000}
+            >
+              Products
+            </Link>
+            <Link
+              className="nav-item"
+              to="services-wrapper"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={1000}
+            >
+              Services
+            </Link>
+            <Link
+              className="nav-item"
+              to="contact-wrapper"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={1000}
+            >
+              Contact
+            </Link>
+          </nav>
+        </header>
+        <div id="about-wrapper">
+          <Title />
+        </div>
+        <div id="products-wrapper">
+          <Products />
+        </div>
+        <div id="services-wrapper">
+          <Services />
+        </div>
+        <div id="contact-wrapper">
+          <Form />
+        </div>
+      </div>
     </div>
   );
 };
