135d49dd70bde47fc261544cc0b3aeedcfccbabd
Richard Tae <taerichard@gmail.com>
2022-10-29 17:14:40 -0700

    aligning and styling cards in mods page

--- src/pages/Mods.js
+++ src/pages/Mods.js
@@ -25,16 +25,14 @@ export default function Mods() {
       </div> */}
       <h1 className="mods-title">Mods</h1>
       <div className="mods-cards">
-        {productsData.map((item) => {
+        {productsData.map((item, index) => {
           return (
             <div className="mods-card">
-              <div className="mods-border">
-                <div className="mods-img">
-                  <img src={item.img} />
-                </div>
-                {/* <p className="mods-name">{item.title}</p>
-                <p className="mods-price">{item.price}</p> */}
+              <div className="mods-img">
+                <img src={item.img} />
               </div>
+              <p className="mods-name">{item.title}</p>
+              <p className="mods-price">{item.price}</p>
             </div>
           );
         })}
